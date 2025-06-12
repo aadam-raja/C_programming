117637f4a32d09617692c84101e7235ba4d0b6d5
Raja Aadam <aadamraja1618@gmail.com>
2025-06-12 12:21:41 +0530

    ADDED FILES

--- incloop.c
+++ incloop.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    
+
     int n;
     printf("ENTER NUMBER");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    int a=0;
-        for (a = 0; a <= n; a++)
-            printf("%d\n", a);
+    int a = 0;
+    for (a = 0; a <= n; a++)
+        printf("%d\n", a);
     return 0;
 }
--- matricesdiagonal.c
+++ matricesdiagonal.c
@@ -1,14 +1,25 @@
-#include<stdio.h>
-int main() {
-int A[3][3];
-int i,j;
-for(i=0;i<3;i++) {
-for(j=0;j<3;j++) {
-printf("ENTER ENTRIES %d %d ",i+1,j+1);
-scanf("%d",&A[i][j]); }}
-for(i=0;i<3;i++) {
-for(j=0;j<3;j++) {
-if(i==j)
-printf("%d",A[i][j]);}
-printf("\n"); }
-return 0;}
+#include <stdio.h>
+int main()
+{
+    int A[3][3];
+    int i, j;
+    int sum = 0;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("ENTER ENTRIES %d %d ", i + 1, j + 1);
+            scanf("%d", &A[i][j]);
+        }
+    }
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (i == j)
+                sum += A[i][j];
+        }
+    }
+    printf("%d", sum);
+    return 0;
+}
